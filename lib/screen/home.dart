eb4a6de7c7ff632027e2681bf2c71817d57ccdc6
Thanakorn004 <tnkzxza@gmail.com>
2026-08-08 14:35:40 +0700

    Step 7 : Button

--- lib/screen/home.dart
+++ lib/screen/home.dart
@@ -5,19 +5,61 @@ class Home extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Column(
-      mainAxisAlignment: MainAxisAlignment.center,
-      crossAxisAlignment: CrossAxisAlignment.stretch,
-      children: [
-        Image.asset('assets/images/sunflower.jpg',width: 200,height: 200,),
-        SizedBox(height: 20,),
-        Image.network(
-          'https://upload.wikimedia.org/wikipedia/commons/thumb/4/40/Sunflower_sky_backdrop.jpg/250px-Sunflower_sky_backdrop.jpg?utm_source=en.wikipedia.org&utm_campaign=parser&utm_content=thumbnail',
-          width: 200,
-          height: 200,
+    return Center(
+      child: Column(mainAxisAlignment: MainAxisAlignment.center, children: [
+        TextButton(
+          style:TextButton.styleFrom(foregroundColor: Colors.pink),
+          onPressed: () {
+            print('Pressed Textbutton');
+          },
+          child: Text(
+            'Chick Me',
+            style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+          ),
         ),
-      ],
+        SizedBox(height: 20),
+        FilledButton(
+          style: FilledButton.styleFrom(
+            foregroundColor: const Color.fromARGB(255, 255, 163, 194),
+            backgroundColor: const Color.fromARGB(255, 58, 160, 255),
+          ),
+          onPressed: () {
+            print('Pressed FilledButton');
+          },
+          child: Text(
+            'Click Me',
+            style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+          ),
+        ),
+        SizedBox(height: 20),
+        OutlinedButton(
+          style: OutlinedButton.styleFrom(
+            foregroundColor: const Color.fromARGB(255, 255, 58, 58),
+            side: BorderSide(color: const Color.fromARGB(255, 121, 4, 255), width: 2,),
+          ),
+          onPressed: () {
+            print('Pressed OutlinedButton');
+          },
+          child: Text(
+            'Click Me',
+            style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+          ),
+        ),
+        SizedBox(height: 20),
+        ElevatedButton(
+          style: ElevatedButton.styleFrom(
+            foregroundColor: const Color.fromARGB(255, 13, 1, 5),
+            backgroundColor: const Color.fromARGB(255, 247, 247, 248),
+          ),
+          onPressed: () {
+            print('Pressed ElevatedButton');
+          },
+          child: Text(
+            'Click Me',
+            style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+          ),
+        )
+      ],),
     );
   }
-}  
-      
+}
