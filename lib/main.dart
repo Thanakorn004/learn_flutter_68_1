2f5927ae7649c778c0893ee6727a9508cbd54c0c
Thanakorn004 <tnkzxza@gmail.com>
2026-08-08 11:24:36 +0700

    Sperate the home widget into its own file

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 
+import 'package:learn_flutter_68_1/screen/home.dart';
+
 void main() {
   // runApp(const MyApp());
 
@@ -23,25 +25,3 @@ void main() {
 }
 
 
-class Home extends StatelessWidget {
-  const Home({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return Center(
-      child: Container(
-        color: Colors.blue,
-        margin: EdgeInsets.symmetric(horizontal: 20, vertical: 10),
-        padding: EdgeInsets.all(50),
-        child: Text(
-          'Hello, World!',
-          style: TextStyle(
-            fontSize: 30,
-            color: Colors.white,
-            fontWeight: FontWeight.bold,
-          ),
-        ),
-      )
-    );
-  }
-}
