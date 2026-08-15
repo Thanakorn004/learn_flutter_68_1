df2c2625ed58c319f6ee5e3f0eae4d9f0508944e
Thanakorn004 <tnkzxza@gmail.com>
2026-08-15 11:10:48 +0700

    Step 10 : Make a model for store data

---
+++ model/person.dart
@@ -0,0 +1,17 @@
+class Person {
+  String name;
+  int age;
+  String job;
+
+
+  Person({required this.name, required this.age, required this.job});
+}
+
+List<Person> persons = [
+  Person(name: "สมชาย", age: 30, job: "นักพัฒนา"),
+  Person(name: "สมหญิง", age: 25, job: "นักออกแบบ"),
+  Person(name: "สมปอง", age: 28, job: "นักการตลาด"),
+  Person(name: "สมศรี", age: 32, job: "นักบัญชี"),
+  Person(name: "สมจิต", age: 27, job: "นักวิจัย"),
+];
+  
