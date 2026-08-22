6a378380522ffc7dfff876a59dcdf87d09144fe9
Thanakorn004 <tnkzxza@gmail.com>
2026-08-22 11:41:16 +0700

    Step 12

--- lib/screen/addFrom.dart
+++ lib/screen/addFrom.dart
@@ -9,44 +9,117 @@ class addform extends StatefulWidget {
 }
 
 class _addformState extends State<addform> {
+  final _formKey = GlobalKey<FormState>();
+  String _name = "";
+  int _age = 20;
+  Job? _job = Job.developer;
+
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Add Person',
-      home: Scaffold(
-        appBar: AppBar(
-          title: Text('Add Person'),
-          backgroundColor: const Color.fromARGB(255, 114, 187, 255),
-          centerTitle: true,
-        ),
-        body: Padding(
-          padding: EdgeInsets.all(20.0),
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Add Person'),
+        backgroundColor: const Color.fromARGB(255, 114, 187, 255),
+        centerTitle: true,
+      ),
+      body: Padding(
+        padding: EdgeInsets.all(20.0),
+        // child: Column(
+        //   children: [
+        //     TextFormField(decoration: InputDecoration(labelText: 'Name')),
+        //     TextFormField(
+        //       decoration: InputDecoration(labelText: 'Age'),
+        //       keyboardType: TextInputType.number,
+        //     ),
+        //     DropdownButtonFormField(
+        //       decoration: InputDecoration(labelText: 'Job'),
+        //       items: Job.values.map((job) {
+        //         return DropdownMenuItem(value: job, child: Text(job.title));
+        //       }).toList(),
+        //       onChanged: (value) {
+        //         print("Selected Job: ${value?.title}");
+        //       },
+        //     ),
+        //     SizedBox(height: 20),
+        //     FilledButton(
+        //       onPressed: () {},
+        //       style: FilledButton.styleFrom(
+        //         backgroundColor: const Color.fromARGB(255, 255, 58, 143),
+        //         padding: EdgeInsets.symmetric(horizontal: 50, vertical: 10),
+        //       ),
+        //       child: Text(
+        //         'Submit',
+        //         style: TextStyle(
+        //           color: const Color.fromARGB(255, 29, 255, 36),
+        //         ),
+        //       ),
+        //     ),
+        //   ],
+        // ),
+        child: Form(
+          key: _formKey,
           child: Column(
             children: [
-              TextFormField(decoration: InputDecoration(labelText: 'Name')),
+              TextFormField(
+                decoration: InputDecoration(labelText: 'Name'),
+                validator: (value) {
+                  if (value == null || value.isEmpty) {
+                    return 'Please enter a name';
+                  }
+                  return null;
+                },
+                onSaved: (value) {
+                  _name = value ?? '';
+                },
+              ),
               TextFormField(
                 decoration: InputDecoration(labelText: 'Age'),
                 keyboardType: TextInputType.number,
+                validator: (value) {
+                  if (value == null || value.isEmpty) {
+                    return 'Please enter an age';
+                  }
+                  return null;
+                },
+                onSaved: (value) {
+                  _age = int.tryParse(value!) ?? 20;
+                },
               ),
-              DropdownButtonFormField(
-                decoration: InputDecoration(labelText: 'Job'),
-                items: Job.values.map((job) {
-                  return DropdownMenuItem(value: job, child: Text(job.title));
+              DropdownButtonFormField<Job>(
+                decoration: InputDecoration(labelText: "Job"),
+                initialValue: _job,
+                items: Job.values.map((key) {
+                  return DropdownMenuItem(value: key, child: Text(key.title));
                 }).toList(),
                 onChanged: (value) {
-                  print("Selected Job: ${value?.title}");
+                  setState(() {
+                    _job = value;
+                  });
                 },
               ),
               SizedBox(height: 20),
               FilledButton(
                 onPressed: () {
-                  
+                  if (_formKey.currentState!.validate()) {
+                    _formKey.currentState!.save();
+                    print("Name $_name, Age $_age, Job ${_job?.title}");
+
+                    setState(() {
+                      people.add(Person(name: _name, age: _age, job: _job!));
+                    });
+
+                    // Navigate back to item after submittion
+                    Navigator.pop(context);
+                  }
                 },
                 style: FilledButton.styleFrom(
-                  backgroundColor: const Color.fromARGB(255, 255, 58, 143),
-                  padding: EdgeInsets.symmetric(horizontal: 50, vertical: 10),
+                  backgroundColor: Colors.amber,
+                  padding: EdgeInsets.symmetric(vertical: 10, horizontal: 50),
                 ),
-                child: Text('Submit', style: TextStyle(color: const Color.fromARGB(255, 29, 255, 36))),
+                child: Text(
+                  "Submit",
+                  style: TextStyle(color: Colors.white, fontSize: 20),
+                ),
               ),
             ],
           ),
@@ -54,4 +127,4 @@ class _addformState extends State<addform> {
       ),
     );
   }
-} 
+}
